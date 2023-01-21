3856be2d6001597f92aa95a5020191640d3bba21
jmcdlungren <j.mcd.lungren@gmail.com>
2023-01-20 21:22:38 -0700

    added variables for questions and answers

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,26 +1,46 @@
-var quizEl = document.querySelector("#questions");
+var questionsEl = document.querySelector("#questions");
+var answersEl = document.querySelector("#answers");
+var li = document.createElement("li");
 
-var questions = [
-    {
-        question: "What HTML element is the JavaScript placed in?",
-        answers: {
-            1: "Section",
-            2: "Script",
-            3: "Head",
-            4: "href"
-        },
-        correctAnswer: '2'
-    },
-    {
-        question: "How do you write 'Hello World' in an alert box?",
-        answers: {
-            1: "alert('Hello World')",
-            2: "alertBox('Hello World')",
-            3: "msgBox('Hello World')",
-            4: "msg('Hello World')"
-        },
-        correctAnswer: '1'
-    }
+
+
+const questions = [
+    "What HTML element is the JavaScript placed in?","How do you write 'Hello World' in an alert box?", "How do you call a function named 'myFunction'?", "How can you add a comment for a single line in JavaScript?", "How can you add a comment that has more than one line?", "JavaScript is the same as Java.", "Is JavaScript case-sensitive?", "How do you declare a JavaScript variable?"
 ];
 
-quizEl.textContent = questions;
+var answr1 = [
+    "Section", "Script", "Head", "href"
+];
+
+var answr2 = [
+    "alert('Hello World')", "alertBox('Hello World')", "msgBox('Hello World')", "msg('Hello World')"
+];
+
+var answr3 = [
+    "function() myFunction()", "myFunction()", "call myFunction()", "call function() myFunction()"
+];
+
+var answr4 = [
+    "/*This is a comment*/", "'This is a comment'", "!!This is a comment", "//This is a comment"
+];
+
+var answr5 = [
+    "//This is a multi-line comment", "!/This is a multi-line comment/!", "/*This is a multi-line comment*/", "/&This is a multi-line comment&/"
+];
+
+var answr6 = [
+    "True", "False"
+];
+
+var answr7 = [
+    "Yes", "No"
+];
+
+var answr8 = [
+    "var quizName", "call var Quiz-Name", "var Quiz-Name", "v quizName"
+];
+
+
+
+
+questionsEl.textContent = questions[3];
